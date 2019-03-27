20d03e176d9302274d034a401efacf7191de3bc2
SimonL <lonelywolf4254@gmail.com>
2019-03-27 10:22:16 -0400

    animation marchent pas bien

--- js/animation.js
+++ js/animation.js
@@ -3,9 +3,10 @@ window.addEventListener('load',function(){
     console.log('passe')
     let elmTitre = document.querySelector('.site-branding .site-title')
     let elmDescription = document.querySelector('.site-branding .site-description')
+    let elmEntete = document.querySelector('.site-featured-image .entry-header .entry-title')
 
+    elmEntete.classList.add('anim4')
     elmTitre.classList.add('anim1')
-    elmTitre.classList.add('anim4')
 
     elmDescription.classList.add('anim3')
 })
